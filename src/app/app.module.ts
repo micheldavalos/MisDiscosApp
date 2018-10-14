acddaa159bc9fc649cbf5e0d2f20ed8861b7de9b
Michel Dávalos <michel.davalos@academicos.udg.mx>
2018-10-14 16:13:03 -0500

    pagina caratula y centrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {CaratulaPage} from "../pages/caratula/caratula";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CaratulaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CaratulaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {CaratulaPage} from "../caratula/caratula";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  caratula1 = CaratulaPage
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  clickCaratula1() {
+    this.navCtrl.push(this.caratula1);
   }
 
 }
